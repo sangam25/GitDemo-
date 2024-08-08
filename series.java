09ffd03ebd2d38280585978eef35bdf22926fa66
sangam25 <37284059+sangam25@users.noreply.github.com>
2024-08-08 13:32:04 +0530

    Add files via upload

---
+++ series.java
@@ -0,0 +1,14 @@
+
+public class series {
+
+	public static void main(String[] args) {
+		
+		       String s="Sangam Kumari"; 
+		       String s1=s.replace("a", "r");
+		       String s2=s.replace("u", "r");
+		       String s3=s.replace("i", "r");
+		       
+		       System.out.print("s");
+
+}
+}
---
+++ toast.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class toast {
+
+	@Test
+	public  void main() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
